c9745dd528ac943702d3bb5990094f010a7d89a5
Christopher <chris.ayling.360@gmail.com>
2018-04-17 15:53:40 +1000

    added report.m

---
+++ report.m
@@ -0,0 +1,44 @@
+%% CAB420 - Machine Learning - Assignment 2
+%
+% Alex Wilson and Christopher Ayling
+%
+
+%% Part A: SVMS and Bayes Classifiers
+
+%% Support Vector Machines
+%%
+% *1.*
+%%
+% *2.*
+
+%% Bayes Classifiers
+%% 
+% *(a)*
+%% 
+% *(b)*
+
+%% Part B: PCA & Clustering
+
+%% Eigen Faces
+%% 
+% *(a)*
+%% 
+% *(b)*
+%% 
+% *(c)*
+%% 
+% *(d)*
+%% 
+% *(e)*
+
+%% Clustering
+%% 
+% *(a)*
+%% 
+% *(b)*
+%% 
+% *(c)*
+%% 
+% *(d)*
+
+
