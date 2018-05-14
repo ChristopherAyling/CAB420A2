7faa7fd0260a65a84099546383ad6fcdbfae9695
Unknown <a123l456e789x@gmail.com>
2018-05-14 13:47:09 +1000

    change decimal to percent in q1

--- report.m
+++ report.m
@@ -19,21 +19,21 @@ load data_ps3_2.mat
 C = 1000;
 
 %Test the SVMs and compare
-% Set 1
+%% Set 1
 svm_test(@Klinear, 1, C, set1_train, set1_test)
 svm_test(@Kpoly, 2, C, set1_train, set1_test)
 svm_test(@Kgaussian, 1, C, set1_train, set1_test)
-% Linear SVM had 0.0446 of test examples misclassified (the lowest of the
+% Linear SVM had 4.46% of test examples misclassified (the lowest of the
 % three). Therefore, for set 1, linear SVM is best. 
 
-% Set 2
+%% Set 2
 svm_test(@Klinear, 1, C, set2_train, set2_test)
 svm_test(@Kpoly, 2, C, set2_train, set2_test)
 svm_test(@Kgaussian, 1, C, set2_train, set2_test)
-% Polynomial (of degree 2) SVM had 0.011 of test examples misclassifies
+% Polynomial (of degree 2) SVM had 1.1% of test examples misclassifies
 % (the lowest of the three). Therefore, for set 2, polynomial SVM is best.
 
-% Set 3
+%% Set 3
 svm_test(@Klinear, 1, C, set3_train, set3_test)
 svm_test(@Kpoly, 2, C, set3_train, set3_test)
 svm_test(@Kgaussian, 1, C, set3_train, set3_test)
